22642d2aa41b830e8845619afc4431e6de48bed3
cha. <charinakate617@gmail.com>
2025-08-25 17:24:10 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from pyscript import document
+
+
+def Operation(event):
+
+
+    num1 = float(document.getElementById("num1").value)
+    num2 =float (document.getElementById("num2").value)
+
+
+    result = (num1 + num2)/ 2 
+
+    document.getElementById("result").innerText = "The Average Of Two digits: " + str(result)
